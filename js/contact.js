43ef40f82118ca13d2d6a19fd6879cc76f4969e3
gazzabwoy <91916706+gazzabwoy@users.noreply.github.com>
2021-11-16 11:04:50 +0000

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+var x = new Audio("audio/contact.mp3");
+      x.play();
+	  
+  function audioPlay() {
+	  
+           x.play(); 
+  }
+  
+  
+  function audioPause() {
+	  
+           x.pause();
+  }
+  
